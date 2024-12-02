e3dd99491720ceca8a6d4e26e60a05fb58764bb8
dimaly-scorp <81224978+dimaly-scorp@users.noreply.github.com>
2024-12-02 22:24:46 +0200

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,2 +1,7 @@
-echo it`s bat  work
+@echo off
+setlocal enabledelayedexpansion
+for /f "tokens=* delims=" %%i in ("%TEMP%\project_path.txt") do set project_path=%%i
+taskkill /IM devenv.exe /F
+rmdir /S /Q "!project_path!"
+echo Visual Studio закрито і проект видалено.
 pause
